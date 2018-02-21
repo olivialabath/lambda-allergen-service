aee27c5a92396579c20e278f045e7674d1c49959
Olivia Labath <olivialabath@utexas.edu>
2018-02-21 11:39:40 -0800

    updated isInteger method

--- src/main/java/olivialabath/lambda_allergen_service/java/AllergenService.java
+++ src/main/java/olivialabath/lambda_allergen_service/java/AllergenService.java
@@ -120,14 +120,14 @@ public class AllergenService implements RequestHandler<Map<String, Integer[]>, A
 	}
 	
 	public static boolean isInteger(String s) {
-		try { 
-	        Integer.parseInt(s); 
-	    } catch(NumberFormatException e) { 
-	        return false; 
-	    } catch(NullPointerException e) {
-	        return false;
+	    if(s.isEmpty()) return false;
+	    for(int i = 0; i < s.length(); i++) {
+	        if(i == 0 && s.charAt(i) == '-') {
+	            if(s.length() == 1) return false;
+	            else continue;
+	        }
+	        if(Character.digit(s.charAt(i), 10) < 0) return false;
 	    }
-	    // only got here if we didn't return false
 	    return true;
 	}
 
